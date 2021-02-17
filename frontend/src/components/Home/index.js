90fa5cbd9340537ddfaddafde1d05a6da919489b
ramses <ramses.romero.jr@gmail.com>
2021-02-17 17:03:45 -0500

    done with the basic basic splash page

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,25 +1,34 @@
 import "./index.css"
 import splash from "./TattooNeon.jpg"
 import {NavLink} from "react-router-dom"
+import SignUpForm from '../SignupFormPage'
+import styled from 'styled-components'
 
+const SignUpFormDiv = styled.div`
+    margin-right:200px;
+`
+
+const Page = styled.div`
+  display:flex;
+  /* align-items:center; */
+  justify-content:space-between;
+`
+const TagLine = styled.div`
+    margin-left:100px;
+    margin-top:30px;
+`
 
 const Home = () =>{
     return (
-    <div>
-        <div id="home-page-container">
-            <img src={splash} />
-            <div id="home-page-overlay" >
-                <div id="home-body">
-                <h1>
-                    Find Your
-                </h1>
-                <NavLink to ="/spots" id="spots-link">
-                    Spot
-                </NavLink>
-                </div>
-            </div>
-        </div>
-    </div>
+    <Page>
+        <TagLine>
+            <h1> Find Your </h1>
+            <NavLink to ="/spots" id="spots-link">Spot</NavLink>
+        </TagLine>
+        <SignUpFormDiv>
+            <SignUpForm />
+        </SignUpFormDiv>
+    </Page>
     )
 };
 
