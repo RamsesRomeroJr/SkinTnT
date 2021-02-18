20a8710d36c9bfc955e4d223d646dfb25eea2f50
ramses <ramses.romero.jr@gmail.com>
2021-02-17 19:58:42 -0500

    added a bit of styling to tattoo spots

--- frontend/src/components/SpotsPage/index.js
+++ frontend/src/components/SpotsPage/index.js
@@ -1,19 +1,35 @@
 import './index.css'
 import {useEffect, useState} from "react"
 import {NavLink} from "react-router-dom"
-
-
+import Spot from './spot.js'
 import {useDispatch, useSelector} from "react-redux"
 import {fetchAllSpots} from "../../store/spots"
+import styled from "styled-components";
 
-const Spot = ({theSpot}) => {
-    return (
-        <div id='spot-link'>
-            <NavLink to={`/spot/${theSpot.id}`} key={theSpot.id}>{theSpot.name}</NavLink>
-            <img src={theSpot.url} id='tattoo-shop-img'/>
-        </div>
-    )
-};
+const SpotsContainer = styled.div`
+    box-sizing:border-box;
+    display: grid;
+    /* grid-template-columns: repeat(auto-fill,240px); */
+    /* justify-content: center; */
+    grid-auto-rows: auto;
+    /* grid-gap: 200px; */
+    margin: 0 auto;
+    width: 50%;
+    padding-top: 15px;
+    padding-bottom: 50px;
+`
+
+const ContainerTitle = styled.h2`
+    font-family: 'Staatliches', cursive;
+    font-size:40px;
+    text-shadow: 0px 1px 3px rgba(0,0,0,0.6);
+
+    &:hover{
+        text-shadow: 0 5px 15px 0px rgba(0,128,0,0.9);
+    }
+
+`
+
 
 const SpotsPage = () =>{
 
@@ -28,12 +44,16 @@ const SpotsPage = () =>{
     }, []);
 
     return (
-        <div id="spots-page">
-            <h2>Tattoo Shops</h2>
-            {!currentSpots && <h3>Loading...</h3>}
-            {currentSpots && currentSpots.map(spot => {
-                return <Spot theSpot={spot} key={spot.id} />
-            })}
+        <div>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <ContainerTitle>Tattoo Shops</ContainerTitle>
+            </div>
+            <SpotsContainer>
+                {!currentSpots && <h3>Loading...</h3>}
+                {currentSpots && currentSpots.map(spot => {
+                    return <Spot theSpot={spot} key={spot.id} />
+                })}
+            </SpotsContainer>
         </div>
     )
 };
---
+++ frontend/src/components/SpotsPage/spot.js
@@ -0,0 +1,37 @@
+import {useEffect, useState} from 'react'
+import {NavLink} from 'react-router-dom'
+import styled from 'styled-components';
+import './index.css'
+
+const TattooImg = styled.img`
+    width: 200px;
+    margin-top:20px;
+    /* height: 200px; */
+`
+
+const SpotContainer = styled.div`
+    background-color: white;
+    border:solid 0.5px lightgrey;
+    box-shadow: 0 1px 5px 0px rgba(0,0,0,0.6);
+    height: 200px;
+    margin-top:20px;
+
+    &:hover{
+        color:#EE6262;
+        border:solid 0.5px lightgrey;
+        box-shadow: 0 3px 9px 0px rgba(238,98,98,0.6);
+    }
+`
+
+const Spot = ({theSpot}) => {
+
+    return (
+        <SpotContainer>
+            <NavLink to={`/spot/${theSpot.id}`} key={theSpot.id} className='spot-title'>{theSpot.name}</NavLink>
+            <div/>
+            <TattooImg src={theSpot.url}/>
+        </SpotContainer>
+    )
+}
+
+export default Spot;
